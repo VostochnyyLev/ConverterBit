77944c0721cb7b07a4ce6fbb6b569d85e80215f8
VostochnyyLev <sasha.yaremenko.00@gmail.com>
2021-12-18 02:40:15 +0200

    Js works, deleted the file and wrote everything in one js file, it counts everything but only from Bitcoin to USD AND UAH

--- js/converter.js
+++ js/converter.js
@@ -1,6 +1,14 @@
-function convert() {
-  const amountBit = document.getElementById("bitcoin").value;
-  const dollarBit = bitCoin(amountBit);
-  const text = `${dollarBit}`;
-  document.getElementById("dollar").innerHTML = text;
+function render() {
+  const bitCoin = document.getElementById("bitcoin").value;
+
+  const rateUa = 1300995;
+  const rateUsd = 48185;
+  let convertedUa = bitCoin * rateUa;
+  let convertedUsd = bitCoin * rateUsd;
+  console.log(bitCoin);
+  console.log(convertedUsd);
+  console.log(convertedUa);
+
+  document.getElementById("dollar").innerHTML = convertedUsd;
+  document.getElementById("gryvna").innerHTML = convertedUa;
 }
